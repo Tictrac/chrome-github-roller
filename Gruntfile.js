6ebc9f5ffd1b197f4447f117cd6d5709bf21a9d9
bobbypates <robert@tictrac.com>
2015-11-19 16:18:29 +0000

    Fix linting issues

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,7 @@
 // Generated on 2015-10-06 using generator-chrome-extension 0.3.1
 'use strict';
 
-// # Globbing
-// for performance reasons we're only matching one level down:
-// 'test/spec/{,*/}*.js'
-// use this if you want to recursively match all subfolders:
-// 'test/spec/**/*.js'
-
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
     // Load grunt tasks automatically
     require('load-grunt-tasks')(grunt);
@@ -284,7 +278,7 @@ module.exports = function (grunt) {
                 options: {
                     archive: function() {
                         var manifest = grunt.file.readJSON('app/manifest.json');
-                        return 'package/github-roller-' + manifest.version + '.zip';
+                        return 'package/chrome-github-roller-' + manifest.version + '.zip';
                     }
                 },
                 files: [{
@@ -297,7 +291,7 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.registerTask('debug', function () {
+    grunt.registerTask('debug', function() {
         grunt.task.run([
             'jshint',
             'concurrent:chrome',
